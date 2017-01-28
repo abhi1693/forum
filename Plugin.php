a40fce27264d1947c092f49f6fb347fa91f16369
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2017-01-29 01:27:37 +0530

    First commit

---
+++ Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Frontend\Forum;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Forum Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Forum',
+            'description' => 'No description provided yet...',
+            'author'      => 'Frontend',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Frontend\Forum\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'frontend.forum.some_permission' => [
+                'tab' => 'Forum',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'forum' => [
+                'label'       => 'Forum',
+                'url'         => Backend::url('frontend/forum/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['frontend.forum.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
